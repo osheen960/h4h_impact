2197b58cba2e2cc6fce3e5f3d4bf0f2faf783182
Osheen Walawedura <osheen960@gmail.com>
2018-10-13 16:36:11 +1100

    js for migration
    js for migration

--- datamerge.js
+++ datamerge.js
@@ -1,1 +1,27 @@
 // js file for datamerge
+import fs from 'fs';
+import path from 'path';
+import promisify from 'es6-promisify';
+import readline from 'readline';
+import GoogleAuth from 'google-auth-library';
+
+const credentialsPath = path.join(__dirname, '.credentials');
+const credentialsFile = path.join(credentialsPath, 'client_secret.json');
+const tokenFile = path.join(credentialsPath, 'token.json');
+
+
+export const getClient = async () => {
+  const credentials = require(credentialsPath);
+
+  const {
+    installed: {
+      client_secret: clientSecret,
+      client_id: clientId,
+      redirect_uris: redirectUrls
+    }
+  } = credentials;
+
+  const redirectUrl = redirectUrls[0];
+
+  const auth = new GoogleAuth();
+  const client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
